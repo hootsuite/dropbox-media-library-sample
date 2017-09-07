a06d81ae098744dbb92af834a41ec2d072157727
Sim Brar <sim.brar@hootsuite.com>
2017-09-07 11:07:25 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# Dropbox Media Library Sample App
+
+> Predefined API Sample app for external developers to install
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [What does this Hootsuite app do?](#what-does-this-hootsuite-app-do)
+- [Getting started](#getting-started)
+  - [Using Heroku](#using-heroku)
+  - [Using another solution](#using-another-solution)
+- [Configuration](#configuration)
+  - [Configuring your Hootsuite App](#configuring-your-hootsuite-app)
+  - [Configuring your shared secret](#configuring-your-shared-secret-for-use-with-attachfiletomessage)
+  - [Providing Media for the Sample App to use](#providing-media-for-the-sample-app-to-use)
+  - [Configuring your OAuth 2 Dev Access Token](#configuring-your-oauth-2-dev-access-token)
+- [Testing your Media Library App with Runscope](#testing-your-media-library-app-with-runscope)
+  - [Setting up Runscope](#setting-up-runscope)
+  - [Customizing the Tests](#customizing-the-tests)
+- [Useful Links](#useful-links)
+
+## Overview
+
+### What does this Hootsuite app do?
+
+This sample app contains a [Media Library](http://app-directory.s3.amazonaws.com/docs/outbound-api/index.html) app. The Media Library enables users with the new compose functionality to pull in images from external sources to use in their social media posts. Media Library apps integrate external media sources into the Media Library, usually using the APIs of cloud services to retrieve a user's media. This app integrates the Media Library with Dropbox using Dropbox's HRA(HTTP(Hypertext Transfer Protocol) REST(Representational State Transfer) API(Application Program Interface)).
+
+The Sample App can be hosted locally, or on Heroku or some other hosting service. The backend is a NodeJS/Express app.
+
+## Getting started
+
+### Using Heroku
+
+1. [Setup Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs#set-up).
+2. Clone this repository.
+3. [Create a Heroku app](https://devcenter.heroku.com/articles/getting-started-with-nodejs#deploy-the-app) for this sample app.
+4. `git push heroku master` to push this app to Heroku. Heroku should detect that this app is a Node/Express app and run your index.js file
+5. Once Heroku says that it's done use `heroku open` and add `/v1/status` to the URL it opened in your browser. If you get a blank page that says `OK` then you've configured Heroku correctly.
+
+## Configuration
+
+### Configuring your Hootsuite App
+
+1. If you already have a Hootsuite developer account head over to [your Hootsuite app  directory management page](https://hootsuite.com/developers/my-apps) and create an app, and inside that app create a media library component. 
+2. Edit the media component and enter the following into the fields: For the media component Service URL use your endpoint's root Heroku domain. `https://<heroku-app-name-here>.herokuapp.com`.
+3. Create a Dropbox app in [Dropbox's My apps](https://www.dropbox.com/developers/apps). Choose the `Dropbox API`, `App folder` access, and give it a name.
+4. Edit your Hootsuite app in [Hootsuite's My Apps](https://hootsuite.com/developers/my-apps). Fill in the field's as following. Left is Dropbox and right is Hootsuite
+  * App key -> Client ID
+  * App secret -> Client secret
+  * `https://api.dropboxapi.com/oauth2/token` -> Token URL
+  * `https://www.dropbox.com/oauth2/authorize` -> Authorization URL	
+5. Hit Save and copy the redirect URI in Hootsuite to Dropbox's redirect URI section.
+6. Install your app by going to your [Hootsuite dashboard](https://hootsuite.com/dashboard) and opening up the Composer (Beta) at the top right. Open the Media Library by pressing the button near the upload area and install your app in the dropdown where `Free Images` is the current selection.
+7. Log into your Dropbox account in the popup (You may need to enable popups on that page).
+8. Next you will need to provide some media for the Media Library to display.
+
+### Providing Media for the Sample App to use
+
+1. Open your Dropbox that you used to login in the Media Library.
+2. There should be a folder called Apps and inside that a folder with the name of the Dropbox App you created. Place all of your media that you want the app to display in that folder. Videos and GIFs are currently not supported.
+
+### Configuring your OAuth 2 Dev Access Token
+
+Note: These steps should only be used for an app in development as your dev token will override the app's auth and use the account of the developer who generates the token.
+1. Create a Dropbox app in [Dropbox's My apps](https://www.dropbox.com/developers/apps). Choose the `Dropbox API`, `App folder` access, and give it a name.
+2. Generate a Dropbox `Generated access token` and save it.
+3. Set it as the environment variable `DROPBOX_ACCESS_TOKEN`. In order to do this on Heroku use the command `heroku config:set DROPBOX_ACCESS_TOKEN=<access token here>`
+
+## Testing your Media Library App with Runscope
+
+Before you can release a Media Library app for use with the Hootsuite platform, your app must pass a set of tests verifying
+that it satisfies our API requirements. You can run these tests yourself using Runscope and the `MediaLibraryAppVerificationTest.json`
+file included in this repo.
+
+Read below for instructions on setting up the tests to work with Runscope.
+
+### Setting up Runscope
+
+In order to use Runscope to run these API tests, you will need a Runscope account. If you don't already have one, sign up
+for an account [here](https://www.runscope.com). After you have an account, follow the steps below to add the tests to your
+account.
+
+1. Download `MediaLibraryAppVerificationTest.json` from this repo.
+2. Login to Runscope and select the `Tests` tab in the top-left.
+3. Click `Import Test` in the bottom left.
+4. You will be taking to a page titled `Import Tests into Cheerful String`. On this page, select `Runscope API Tests`. Then,
+add the `MediaLibraryAppVerificationTest.json` file that you downloaded previously.
+5. Click `Import Tests`, and then go back to the `Tests` tab. You should see a new box labeled `Media Library App Verification
+Test`.
+
+Next, you will have to customize the test to work with your app.
+
+### Customizing the Tests
+
+After you've imported the test suite into Runscope, you will need to change the settings to work with your app. Here are instructions
+for doing that:
+
+1. Select the `Tests` tab in the upper-left, then hover over the `Media Library App Verification Test` that you just imported.
+Click the `Edit` button that will appear in the lower-left corner of the box.
+2. Under the `Environment` header, click the arrow next to `Test Settings` to expand it. The `Initial Variables` tab should
+already be selected.
+3. Click `Add Initial Variable`. We will be adding four variables to the tests. All variable names are case sensitive.
+4. The first variable is named `baseURL`. For the value, enter the URL that your app has exposed for API calls.
+5. The second variable is named `supportedMediaTypes`. For the value, copy and paste in this array: `['Image', 'AnimatedGif', 'Video', Folder']`.
+Then, delete the variables that your app does not support from that array. For example, if your app only searches for images
+and gifs, you would delete `'Video'` and `'Folder'` from the array, so that the value reads `['Image', 'AnimatedGif']`. This step
+will prevent the test suite from running tests on functionality that you don't support.
+6. The third variable is named `authToken`. For the value, enter a valid access token for your app. If your app does not 
+support authentication, just enter a dummy value for this variable. (You need some value for this variable, even if your 
+app does not support authentication, or otherwise the tests will break.)
+7. The final variable is named `nextCursor`. It should always be set to `defaultCursorValue`. (The tests will automatically
+change this variable as they run, but you don't need to do anything yourself.)
+8. At this point, the tests are fully configured. Click `Save & Run` near the top of the page in order to run the tests
+against your app!
+
+## Useful Links
+
+* [Hootsuite Developer Docs](https://developer.hootsuite.com/docs)
+* [Dropbox Developer Docs](https://www.dropbox.com/developers)
---
+++ index.js
@@ -0,0 +1,299 @@
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const app = express();
+const request = require('request');
+
+const dropboxApiUrl = 'https://api.dropboxapi.com/2';
+const dropboxContentUrl = 'https://content.dropboxapi.com/2';
+const compatibleExtensions = {
+  'Image' : ['jpg', 'jpeg', 'png'],
+  'Video': ['mp4'],
+  'AnimatedGif': ['gif'],
+  'Folder': [],
+  // GIFs are currently not supported by Dropbox
+  // Videos are currently not support by Hootsuite
+  '': ['jpg', 'jpeg', 'png']
+};
+
+const mimeTypes = {
+  'jpeg': 'image/jpeg',
+  'jpg': 'image/jpeg',
+  'png': 'image/png',
+  'gif': 'image/gif',
+  'mp4': 'video/mp4'
+};
+const mediaTypes = {
+  'jpeg': 'Image',
+  'jpg': 'Image',
+  'png': 'Image',
+  'mp4': 'Video',
+  'gif': 'AnimatedGif',
+};
+
+class Media {
+  constructor (height = 0, width = 0, url = '', name = '') {
+    this.id = '';
+    this.name = name;
+    var extension = getExtension(name);
+    this.mediaType = mediaTypes[extension];
+    this.mimeType = mimeTypes[extension];
+    this.original = {
+      url: url,
+      width: height,
+      height: width,
+      sizeInBytes: 0
+    };
+    this.thumbnail = {
+      url: url,
+      width: width,
+      height: height,
+    };
+  }
+}
+
+class Metadata {
+  constructor (next = '', previous ='') {
+    this.cursor = {
+        next: next,
+        previous: previous
+    };
+  }
+}
+
+var flatten = arr => {
+  return arr.reduce((flat, toFlatten) => {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+};
+
+var getExtension = fileName => {
+  var re = /(?:\.([^.]+))?$/;
+  return re.exec(fileName)[1];
+};
+
+var getDropboxDirectUrl = (fileId, authHeader) => {
+  return new Promise((resolve, reject) => {
+    var options = {
+      url: `${dropboxApiUrl}/files/get_temporary_link`,
+      json: true,
+      body: {
+        path: fileId
+      },
+      headers: {
+        'Authorization': authHeader
+      }
+    };
+    request.post(options, (err, response, body) => {
+      resolve({
+        id: fileId,
+        sharedLink: body.link
+      });
+    });
+  });
+};  
+
+var innerDropboxThumbnails = (fileIds, width, height, authHeader, promises) => {
+  var currSlice = fileIds.slice(0, 24);
+  var nextSlice = fileIds.slice(25);
+  var entries = currSlice.map( fileId => {
+    return {
+      path: fileId,
+      format: 'jpeg',
+      size: `w${width}h${height}`
+    };
+  });
+  var options = {
+    url: `${dropboxContentUrl}/files/get_thumbnail_batch`,
+    json: true,
+    body: {
+      entries: entries
+    },
+    headers: {
+      'Authorization': authHeader
+    }
+  };
+  promises.push(new Promise((resolve, reject) => {
+    request.post(options, (err, response, body) => {
+      resolve(body.entries.map(entry => {
+        return {
+          id: entry.metadata.id,
+          thumbnail: entry.thumbnail
+        };
+      }));
+    });
+  }));
+
+  if (nextSlice.length == 0) {
+    return Promise.all(promises).then();
+  } else {
+    return innerDropboxThumbnails(nextSlice, width, height, authHeader, promises);
+  }
+};
+
+var flatten = arr => {
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+};
+
+var getExtension = fileName => {
+  var re = /(?:\.([^.]+))?$/;
+  return re.exec(fileName)[1];
+};
+
+var getDropboxThumbnails = (fileIds, width, height, authHeader) => {
+  return new Promise((resolve, reject) => {
+    resolve(innerDropboxThumbnails(fileIds, width, height, authHeader, []).then());
+  });
+};
+
+var getItemsWithFilter = (folderName = '', authHeader = '', query = '', mediaType = '', cursor) => {
+  return new Promise((resolve, reject) => {
+    var options = {
+      url: `${dropboxApiUrl}/files/list_folder`,
+      json: true,
+      body: { },
+      headers: {
+        'Authorization': authHeader
+      }
+    };
+    if (cursor) {
+      options.body = {
+        cursor: cursor
+      };
+      options.url += '/continue';
+    } else {
+      options.body = {
+        path: folderName,
+        recursive: false,
+        include_media_info: true,
+        include_deleted: false,
+        include_has_explicit_shared_members: false,
+        include_mounted_folders: true,
+      };
+      if (query !== '') {
+        // search all folders/files if there is a query because you can't currently specify folders
+        options.body.recursive = true;
+      } else {
+        // load a maximum of 20 items at at time
+        options.body.limit = 20;
+      }
+    }
+    
+    request.post(options, (err, response, body) => {
+      var folders = body.entries
+      .filter(item => {
+        return item['.tag'] === 'folder' && (mediaType === 'Folder' || mediaType === '');
+      })
+      // filter for query
+      .filter(item => {
+        return item.name.toLowerCase().includes(query.toLowerCase());
+      });
+      var compatibleFiles = body.entries
+      // filter out files with filesizes larger than 5 MB
+      .filter(item => {
+        return item.size <= 5 * 1000 * 1000;
+      })
+      // filter for mediaType
+      .filter(item => {
+        return compatibleExtensions[mediaType].includes(getExtension(item.name));
+      })
+      // filter for query
+      .filter(item => {
+        return item.name.toLowerCase().includes(query.toLowerCase());
+      });
+      var promises = [];
+      compatibleFiles.forEach(file => {
+        promises.push(getDropboxDirectUrl(file.id, authHeader));
+      });
+      promises.push(getDropboxThumbnails(compatibleFiles.map(file => { return file.path_lower; }), 640, 480, authHeader));
+      Promise.all(promises).then(data => {
+        var flatData = flatten(data);
+        var fileData = folders.map(folderData => {
+          var folder = new Media();
+          folder.id = folderData.id;
+          folder.name = folderData.name;
+          folder.mediaType = 'Folder';
+          folder.mimeType = 'application/vnd.hootsuite.folder';
+          return folder;
+        });
+        fileData.push(...compatibleFiles.map(file => {
+          var filteredData = flatData.filter(item => {
+            return file.id === item.id;
+          });
+          const fileData = Object.assign({}, ...filteredData);
+          var media = new Media(file.media_info.metadata.dimensions.height, file.media_info.metadata.dimensions.width, fileData.sharedLink, file.name);
+          media.id = file.id;
+          media.original.sizeInBytes = file.size;
+          media.thumbnail.url = 'data:image/jpeg;base64, ' + fileData.thumbnail;
+          return media;
+        }));
+        // body.cursor is the next cursor, cursor is the current cursor
+        var metadata = new Metadata(body.cursor, cursor);
+        // don't set another cursor if it points to nothing
+        if (fileData.length === 0 || body.has_more === false) {
+          metadata.cursor.next = '';
+        }
+        resolve({
+          fileData: fileData,
+          metadata: metadata
+        });
+      });
+    });
+  });
+};
+
+app.use((req, res, next) => {
+  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+  return next();
+});
+
+app.use(express.static('static'));
+
+app.get('/v1/status', (req, res) => {
+  res.status(200).end();
+});
+
+app.get('/v1/media', (req, res) => {
+  var dropboxAuthHeader = req.headers.authorization;
+  try {
+    // Once OAuth 2 w/ Dropbox redirect is fixed uncomment this if statement
+    // if (!dropboxAuthHeader) {
+    dropboxAuthHeader = 'Bearer ' + process.env.DROPBOX_ACCESS_TOKEN;
+    // }
+  } catch (err) {
+    console.log(err);
+  }
+  if (dropboxAuthHeader) {
+    try {
+      getItemsWithFilter(req.query.parentId, dropboxAuthHeader, req.query.query, req.query.mediaType, req.query.cursor).then(data => {
+        res.json({
+          data: data.fileData,
+          metadata: data.metadata
+        });
+      });
+    } catch (err) {
+      res.status(500).send({error: 'error occurred while fetching items'});
+    }
+  } else {
+    res.status(500).send({error: 'dropbox auth header not found'});
+  }
+});
+
+// All Hoosuite apps require HTTPS, so in order to host locally
+// you must have some certs. They don't need to be issued by a CA for development,
+// but for production they definitely do! Heroku adds its own TLS,
+// so you don't have to worry about it as long as TLS is enabled on your Heroku app.
+if (fs.existsSync('certs/server.crt') && fs.existsSync('certs/server.key')) {
+  const certificate = fs.readFileSync('certs/server.crt').toString();
+  const privateKey = fs.readFileSync('certs/server.key').toString();
+  const options = {key: privateKey, cert: certificate};
+
+  var server = https.createServer(options, app).listen(process.env.PORT || 5000);
+  console.log(`Example app listening on port ${process.env.PORT || 5000} using HTTPS`);
+} else {
+  var server = http.createServer(app).listen(process.env.PORT || 5000);
+  console.log(`Example app listening on port ${process.env.PORT || 5000}`);
+}
